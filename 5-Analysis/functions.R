0f37ff45027104cd5ddb0c2228e23e88b06e25bd
hnguyen19 <huong@iastate.edu>
2022-01-31 23:18:32 -0600

    check code operation in indivdual .Rmd's for analysis and imputation

--- 5-Analysis/functions.R
+++ 5-Analysis/functions.R
@@ -1,139 +1,107 @@
 ## The codes are ordered chronologically. 
 
 library(patchwork)
+library(emmeans)
 
+###~~~~~~~~~~ File name: Population-sex-biom-dens.Rmd ~~~~~~~~###
+### Effects of crop ID, corn weed management, population aboveground mass, population density, and their interaction (when applicable) on population sex ratio. 
 
-###~~~~~~~~~~ Population Biomass, Density, Sex ratio, and their relationship ~~~~~~~~###
-
-## comparison arrows for ANOVA
-arrow_p <- function(data){
-  plot(data, comparisons = T) + 
+### Figure 2 was made with a customized function based on emmeans::plot() 
+arrow_all <- function(data){
+  plot(data, comparisons = TRUE) + 
     theme_bw() + 
-    theme(text=element_text(size=20))+
+    theme(text=element_text(size=10))+
     coord_flip() +
-    #  xlab("Population biomass (g/m2)") +
-    ylab("Crop ID")
+    ylab("Crop ID") # xlab to be filled at each plot
 }
 
-arrow_p <- function(data){
-plot(data, comparisons = TRUE, by = "Corn_weed_management") + 
-  theme_bw() + 
-  theme(text=element_text(size=10),
-        strip.text.x = element_text(size = 5))+
-  coord_flip() +
-  geom_vline(xintercept=0.5, size=0.5, linetype="dashed") + 
-  #ggtitle(expression((A)~1~plant/m^2))  +
-  xlab("Female proportion") +
-  ylab("Crop ID") 
-}
-## ## comparison arrows for ANOVA with density covar
 
-arrow_p_dens <- function(data){
-  plot(data, comparisons = TRUE, by = "Corn_weed_management") + 
+### Figure 3 was made with a customized function based on emmeans::plot() 
+arrow_split <- function(data){
+  plot(data, comparisons = TRUE, by = "Corn_weed_management") +  #facet by corn_weed_management
     theme_bw() + 
     theme(text=element_text(size=10),
           strip.text.x = element_text(size = 5))+
     coord_flip() +
     geom_vline(xintercept=0.5, size=0.5, linetype="dashed") + 
-    #ggtitle(expression((A)~1~plant/m^2))  +
     xlab("Female proportion") +
-    ylab("Crop ID") 
+    ylab("Crop ID") + 
+    facet_wrap(~ Corn_weed_management, 
+               labeller = custom_labels, strip.position = "right", ncol = 1) #remove "_" in "corn_weed_management"
 }
 
-#print p-values as is  
 
-
-## scatter not working yet
-scatter_p <- function(data, x, y, z, t){
-  ggplot(prop_biom18, aes(x, y)) + 
-    geom_point(aes(col = z, size = 3))+
-    geom_smooth(aes(fill=z, col = z)) +
-    facet_wrap(t, scales = "free_x") +
-    theme_bw() +
-    theme(text=element_text(size=15))+
-    scale_size(guide=FALSE)
-}
-
-#add points from the testing set with another geom_point
-
-# visual evaluation  https://rpubs.com/therimalaya/43190
-
-
-
-## for pooled analysis of imputed data sets, without covariate    
-# Chi-squared is not appropriate for quasibinomial, F-test is
-mod_type1 = function(dat) {
-  mod = glm(formula = cbind(Female, Male) ~ Block + Crop_ID + Corn_weed_management + Crop_ID:Corn_weed_management,
-            data=dat, family=quasibinomial(link = "logit"))
-  anova(mod,  test = "F")[-1,]
-}
-
-#Type 3 ANOVA 
+### Type 3 ANOVA  ###
+#Figure 4, panel A: to test the effects of crop identity and corn weed management on sex ratio using 24 imputed data sets 
 mod_jt = function(dat) {
-  mod = glm(formula = cbind(Female, Male) ~ Block + Crop_ID + Corn_weed_management + Crop_ID:Corn_weed_management,
+  mod = glm(formula = cbind(Female, Male) ~ Block +
+              Crop_ID*Corn_weed_management,
             data=dat, family=quasibinomial(link = "logit"))
-  # emm = emmeans(mod, c("Crop_ID", "Corn_weed_management", "Block"),data = dat, type = "response")
+  # emm = emmeans(mod, c("Crop_ID", "Corn_weed_management", "Block"), data = dat, type = "response")
   joint_tests(mod, test = "F")
 }
 
-# summary for deviance and overdispersion inspection  
+# Figure 4, panel B: to test the effects of crop identity and corn weed management on sex ratio using 24 imputed data sets 
+#A customized function from emmeans::emmeans()
+mod_emm_crop_ID = function(dat) {
+  mod = glm(formula = cbind(Female, Male) ~ Block + 
+              Crop_ID*Corn_weed_management,
+            data=dat, family=quasibinomial(link = "logit"))
+  emmeans(mod, "Crop_ID" ,  #mean sex ratio at each Crop_ID, averaged over corn_weed_management.
+          data = dat,
+          type = "response",
+          infer = c(TRUE,TRUE))
+}
+
+
+###~~~~~~~~~~ File name: sex-29-imputation-jan29-22.Rmd  ~~~~~~~~~~###
+## Appendix B: 2019 sex ratio imputation
+## Extract ANOVA tables for all imputed data sets by applying `mod_jt` on all the sets. This function was used to identify which imputed data sets allow for estimation of corn weed management effect on sex ratio (full sets, denoted "n" in Appendix B). ANOVA tables from the full sets have 5 columns (source of variation, df1, df2, F-value, and p-value), while ANOVA tables from the not full sets have an additional column called Note. 
+
+## Table 11: Numeric diagnosis for pooled analysis of imputed data sets, with no  covariate 
 
 mod_sum = function(dat) {
-  mod = glm(formula = cbind(Female, Male) ~ Block + Crop_ID + Corn_weed_management + Crop_ID:Corn_weed_management,
+  mod = glm(formula = cbind(Female, Male) ~ Block +
+              Crop_ID*Corn_weed_management,
             data=dat, family=quasibinomial(link = "logit"))
   # emm = emmeans(mod, c("Crop_ID", "Corn_weed_management", "Block"),data = dat, type = "response")
   data.frame(Deviance  = c(summary(mod)$deviance),
              Dispersion = c(summary(mod)$dispersion))
 }
 
-# <https://stackoverflow.com/questions/57373775/how-to-save-the-output-summarymodel-of-generalized-linear-model-as-a-csv-fi> 
+## Table 11: Numeric diagnosis for pooled analysis of imputed data sets, with population density covariate 
 
-
-### follow-up emm
-
-mod_emm_x = function(dat) {
-  mod = glm(formula = cbind(Female, Male) ~ Block + Crop_ID + Corn_weed_management + Crop_ID:Corn_weed_management,
+sexr19_dens_sum  = function(dat) {
+  mod = glm(cbind(Female, Male) ~ log(plant_m_sq + 0.025)  + Block + Crop_ID + Corn_weed_management +
+              Crop_ID:Corn_weed_management + 
+              log(plant_m_sq + 0.025):Crop_ID +
+              log(plant_m_sq + 0.025):Corn_weed_management +
+              log(plant_m_sq + 0.025):Crop_ID:Corn_weed_management ,
             data=dat, family=quasibinomial(link = "logit"))
-  print(emmeans(mod, c("Crop_ID", "Corn_weed_management"), data = dat, type = "response", infer = c(T,T)), export = T)
-}
+  data.frame(Deviance  = c(summary(mod)$deviance), 
+             Dispersion = c(summary(mod)$dispersion))
+}  
 
-# emm averaged over corn herbicide regimes
-mod_emm_h = function(dat) {
-  mod = glm(formula = cbind(Female, Male) ~ Block + Crop_ID + Corn_weed_management + Crop_ID:Corn_weed_management,
-            data=dat, family=quasibinomial(link = "logit"))
-  emmeans(mod, "Crop_ID" ,data = dat, type = "response", infer = c(T,T))
-}
+## for pooled analysis of imputed data sets, with population biomass covariate  
 
+## Table 11 - Appendix B: Numeric diagnosis for pooled analysis of imputed data sets, with population aboveground mass covariate 
+## non-zero adjustment of half the smallest non-zero values in g_m_sq column
 
-
-### grid  
-mod_grid = function(dat) {
-  mod = glm(formula = cbind(Female, Male) ~ Block + Crop_ID + Corn_weed_management + Crop_ID:Corn_weed_management,
-            data=dat, family=binomial)
-  m_grid = ref_grid(mod)
-  grid_crop = add_grouping(m_grid, "Crop", "Crop_ID", Crop)
-  emmeans(grid_crop, c("Crop", "Corn_weed_management"), type = "response", infer = c(T,T))
-}
-
-mod_pwpp = function(dat) {
-  mod = glm(formula = cbind(Female, Male) ~ Block + Crop_ID + Corn_weed_management + Crop_ID:Corn_weed_management,
-            data=dat, family=binomial)
-  m_grid = ref_grid(mod)
-  grid_crop = add_grouping(m_grid, "Crop", "Crop_ID", Crop)
-  grid_crop_emm = emmeans(grid_crop, c("Crop", "Corn_weed_management"), type = "response")
-  pwpp(grid_crop_emm, type ="response", by = "Corn_weed_management" , comparisons = TRUE)
+sexr19_biom_sum  = function(dat) {
+  mod =  glm(cbind(Female, Male) ~ log(g_m_sq + 0.002)  + Block +
+               Crop_ID + Corn_weed_management +
+               Crop_ID:Corn_weed_management + 
+               log(g_m_sq + 0.002):Crop_ID +
+               log(g_m_sq + 0.002):Corn_weed_management +
+               log(g_m_sq + 0.002):Crop_ID:Corn_weed_management, 
+             data=dat,family=quasibinomial(link = "logit"))
+  data.frame(Deviance  = c(summary(mod)$deviance), 
+             Dispersion = c(summary(mod)$dispersion))
 }
 
 
-
-
-###~~~~~~~~~~ Individual biomass, fecundity and their relationship ~~~~~~~~###
-
-
-
-
-
-#### Appendix C: Diagnosis plots in ggplot2 style. 
+###~~~~~~~~~~ Appendix C: Diagnosis plots in ggplot2 style. ~~~~~~~~~~###
+## Appendix C is not included in the manuscript, but presented here for model diagnosis reference 
 
 ### gls data here need to be augmented first 
 # predicted vs residual 
@@ -188,7 +156,7 @@ diag_seed <- function(data, tag){
  
 }
 
-## Diagnose a gls model to assess the effects of crop identity and corn weed management on individual biomass 
+## Diagnose a gls model used to assess the effects of crop identity and corn weed management on individual biomass 
 diag_biom <- function(data, tag){
   aug_data <- augment(data)
   fit_resid <-  ggplot(aug_data, aes(x =  .fitted, y = .resid)) +
@@ -234,64 +202,4 @@ diag_biom <- function(data, tag){
     theme(plot.title = element_text(face = "bold")) 
   return((fit_resid / qq / obs_resid / resid_h)) 
 }
-
-### Extract ANOVA tables for all imputed data sets. This function was used to identify which imputed data sets allow for estimation of corn weed management effect on sex ratio (full sets, denoted "n" in Appendix B). ANOVA tables from the full sets have 5 columns (source of variation, df1, df2, F-value, and p-value), while ANOVA tables from the not full sets have an additional column called Note. 
-sexr19_dens_glm3  = function(dat) {
-  mod = glm(cbind(Female, Male) ~ log(plant_m_sq + 0.025)  + Block +
-              Crop_ID + Corn_weed_management +
-              Crop_ID:Corn_weed_management + 
-              log(plant_m_sq + 0.025):Crop_ID +
-              log(plant_m_sq + 0.025):Corn_weed_management +
-              log(plant_m_sq + 0.025):Crop_ID:Corn_weed_management ,
-            data=dat, family=quasibinomial(link = "logit"))
-  joint_tests(mod, test = "F")
-}
-
-## Table 11 - Appendix B: Numeric diagnosis for pooled analysis of imputed data sets, with population density covariate 
-
-sexr19_dens_sum  = function(dat) {
-  mod = glm(cbind(Female, Male) ~ log(plant_m_sq + 0.025)  + Block + Crop_ID + Corn_weed_management +
-              Crop_ID:Corn_weed_management + 
-              log(plant_m_sq + 0.025):Crop_ID +
-              log(plant_m_sq + 0.025):Corn_weed_management +
-              log(plant_m_sq + 0.025):Crop_ID:Corn_weed_management ,
-            data=dat, family=quasibinomial(link = "logit"))
-  data.frame(Deviance  = c(summary(mod)$deviance), 
-             Dispersion = c(summary(mod)$dispersion))
-}  
-
-## for pooled analysis of imputed data sets, with population biomass covariate  
-
-## Table 11 - Appendix B: Numeric diagnosis for pooled analysis of imputed data sets, with population aboveground mass covariate 
-## non-zero adjustment of half the smallest non-zero values in g_m_sq column
-
-sexr19_biom_sum  = function(dat) {
-  mod =  glm(cbind(Female, Male) ~ log(g_m_sq + 0.002)  + Block +
-               Crop_ID + Corn_weed_management +
-               Crop_ID:Corn_weed_management + 
-               log(g_m_sq + 0.002):Crop_ID +
-               log(g_m_sq + 0.002):Corn_weed_management +
-               log(g_m_sq + 0.002):Crop_ID:Corn_weed_management, 
-             data=dat,family=quasibinomial(link = "logit"))
-  data.frame(Deviance  = c(summary(mod)$deviance), 
-             Dispersion = c(summary(mod)$dispersion))
-}
-
-### Extract ANOVA tables for all imputed data sets. This function was used to identify which imputed data sets allow for estimation of corn weed management effect on sex ratio (full sets, denoted "n" in Appendix B). ANOVA tables from the full sets have 5 columns (source of variation, df1, df2, F-value, and p-value), while ANOVA tables from the not full sets have an additional column called Note.
-
-#### As of the current data size, community density is a better covariate than community cumulative aboveground mass in estimating sex ratio at different treatments
-
-## non-zero adjustment of half the smallest non-zero values in plant_m_sq column
-
-sexr19_biom_glm3 = function(dat){
-  mod =  glm(cbind(Female, Male) ~ log(g_m_sq + 0.0025) + Block +
-               Crop_ID + Corn_weed_management +
-               Crop_ID:Corn_weed_management + 
-               log(g_m_sq + 0.002):Crop_ID +
-               log(g_m_sq + 0.002):Corn_weed_management +
-               log(g_m_sq + 0.002):Crop_ID:Corn_weed_management,
-             data=dat, family=quasibinomial(link = "logit"))
-  joint_tests(mod, test = "F")
-}
-
 
